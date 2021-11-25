53ebe8792eacc65bab9ab88e45523deda14cfc64
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2021-11-25 13:51:14 +0100

    uniform variables

--- HLSLForCSharpExample/Program.cs
+++ HLSLForCSharpExample/Program.cs
@@ -14,7 +14,7 @@ namespace HLSLForCSharpExample
             for (int i = 0; i < inputData.Length; i++)
             {
                 inputData[i] = i;
-                multipliers[i] = i;
+                multipliers[i] = 9 -    i;
                 Console.WriteLine(i);
             }
             Console.WriteLine();
@@ -22,7 +22,8 @@ namespace HLSLForCSharpExample
             //use compute shader
             cs.Stage(); // only needed once when this is the only shader that you use
             cs.SetRWStructuredBuffer(inputData, sizeof(int), 0); // Create read write buffer
-            cs.SetStructuredBuffer(multipliers, sizeof(int), 0); // Create read-only buffer
+            cs.SetStructuredBuffer(multipliers, sizeof(int), 1); // Create read-only buffer
+            cs.SetUniformVariable(2, sizeof(int), 0); // Set uniform variable
             cs.DispatchCompute(5, 1, 1); // as the groupsize is 2 we only need to instance 5 groups on the x axis
             int[] outputData = cs.GetParsedRWBuffer<int>(0); // get output from GPU
             cs.UnStage(); // not needed when this is the only shader that you use
--- HLSLForCSharpExample/ShaderExample/Shader.hlsl
+++ HLSLForCSharpExample/ShaderExample/Shader.hlsl
@@ -1,10 +1,11 @@
 
 
 RWStructuredBuffer<int> buffer;
-StructuredBuffer<int> buffer2;
-//uniform int multiplier;
+uniform int multiplier : register(t0);
+StructuredBuffer<int> buffer2 : register(t1);
+
 [numthreads(2, 1, 1)]
 void CSMain(uint3 id : SV_DispatchThreadID)
 {
-    buffer[id.x] = buffer[id.x] * buffer2[id.x];
+    buffer[id.x] = buffer[id.x] * buffer2[id.x] * multiplier;
 }
